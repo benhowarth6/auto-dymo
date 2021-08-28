4f2f111d3517675de7245ea207a025c7b09b71ce
David Cole <dxcrcc@rit.edu>
2021-08-27 20:58:39 -0400

    Server logging now reflects testing mode being active

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const app = require('./app');
 const getDateTime = require('./lib/util/getDateTime');
+var constants = require('./lib/constants')
 
 //Can overrwite port with an ENV var, otherwise will use 3000
 const port = process.env.PORT || 3000;
@@ -9,5 +10,10 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 //Listen on port (3000)
-console.log("\n\n" + getDateTime() + " Server started and listening on port " + port + ":");
+
+if(!constants.testing) console.log("\n\n" + getDateTime() + " Server started and listening on port " + port + ":");
+else{
+    const style = 'font-weight: bold'
+    console.log("%c\n\n" + getDateTime() + " Server started in testing mode and listening on port " + port + ":", style);
+}
 server.listen(port);
