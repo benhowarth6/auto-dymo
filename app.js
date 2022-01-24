a2702bbd0e032be4f0890e1e3733660b0c326aba
DavidArthurCole <40234707+DavidArthurCole@noreply.github.com>
2022-01-24 13:21:51 -0500

    Zoop

--- app.js
+++ app.js
@@ -1,59 +1,39 @@
-const checkAuth = require('./lib/util/checkAuth');
 const express = require('express');
 const app = express();
-
+const printRoutes = require('./lib/routes/print');
+const getDateTime = require('./lib/util/getDateTime');
+const returnCode = require('./lib/util/returnCode');
 const logger = require('./lib/util/logger');
 
+//Route any /print requests
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
-const getDateTime = require('./lib/util/getDateTime');
-
-//Route any /print requests
-const printRoutes = require('./lib/routes/print');
 app.use('/print', printRoutes);
 
-const returnCode = require('./lib/util/returnCode');
-
-//Scraper deterrent
-app.use('/favicon.ico', (req, res, next) => {
-
-  //Log the IP of invalid requests - skimmer catching
-  var logIp = req.socket.remoteAddress.replace("::ffff",'');
-  
-  logger.log('info', "\n" + getDateTime() + " Scraper: " + logIp);
-  returnCode(404, res, req.body, ":(")
+//Log the IP of invalid requests - skimmer catching
+app.use('/favicon.ico', (req, res) => {
+  logger.log('info', "\n" + getDateTime() + " Scraper: " + req.socket.remoteAddress.replace("::ffff",''));
+  returnCode(204, res, req.body, '');
 })
 
-app.use('/', (req, res, next) => {
-
-  //Log the IP of invalid requests - skimmer catching
-  var logIp = req.socket.remoteAddress.replace("::ffff",'');
-
-  if(checkAuth(res, req, "")){
-    returnCode(400, res, req.body, console)
-  }
-
-  logger.log('info', "\n" + getDateTime() + " Scraper: " + logIp);
-  returnCode(404, res, req.body, ":(")
+//Log the IP of invalid requests - skimmer catching
+app.use('/', (req, res) => {
+  logger.log('info', "\n" + getDateTime() + " Scraper: " + req.socket.remoteAddress.replace("::ffff",''));
+  returnCode(204, res, req.body, '');
 })
 
 //Any request that makes it here is ~no bueno~
 app.use((req, res, next ) => {
-
-  //Log the IP of invalid requests - skimmer catching
-  var logIp = req.socket.remoteAddress.replace("::ffff",'');
-
   //Log the time and IP
-  logger.log('info', "\n\n" + getDateTime() + " Invalid request from " + logIp + ":");
+  logger.log('info', "\n\n" + getDateTime() + " Invalid request from " + req.socket.remoteAddress.replace("::ffff",'') + ":");
 
-  const error = new Error('Invalid request.');
+  const error = new Error(':(');
   error.status = 404;
   next(error);
 })
 
 //If you get a 500 back, something is wrong with the code itself.
-app.use((error, req, res, next) => {
+app.use((error, res) => {
   res.status(error.status || 500);
   res.json({
       error: {
