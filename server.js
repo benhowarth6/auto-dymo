4c15277713085bf582a57125a422624b48fd9889
DavidArthurCole <40234707+DavidArthurCole@noreply.github.com>
2022-03-29 12:15:58 -0400

    Added HTTPS capabilities. Updated README to reflect this

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
-var constants = require('./lib/util/other/constants');
+const constants = require('./lib/util/other/constants');
 const logger = require('./lib/util/other/logger');
 
 //Can overrwite port with an ENV var, otherwise will use 3000
 const port = process.env.PORT || 3000;
 
+//Load SSL cert and key for HTTPS config
+const serverOptions = constants.protocol == 'https' ? {
+    key: fs.readFileSync(__dirname + '/certs/autodymo.key'),
+    cert: fs.readFileSync(__dirname + '/certs/autodymo.crt')
+} : {};
+
 //Create a server using app.js
-const server = http.createServer(app);
+const server = (constants.protocol == 'http' ? http.createServer(app) : https.createServer(serverOptions, app));
 
 const ConsoleWindow = require("node-hide-console-window");
 
 //Listen on port (3000)
-logger.log('info', '\n');
-logger.log('info', "----------------------------------------------------------------");
-logger.log('info', "----------------------------------------------------------------");
-if(!constants.testing) logger.log('info', "\n\nServer started and listening on port " + port + ":");
-else logger.log('info', "\n\nServer started [IN TESTING MODE] mode and listening on port " + port + ":");
+logger.log('info', "\n----------------------------------------------------------------".repeat(2));
+logger.log('info', "\n\nServer (" + (constants.protocol.toString().toUpperCase()) + ") started" + (constants.testing ? " [IN TESTING MODE]" : "")  + " and listening on port " + port + ":");
 server.listen(port);
 
 if(!constants.testing) ConsoleWindow.hideConsole();
